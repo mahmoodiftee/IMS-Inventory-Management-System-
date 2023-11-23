1fdc5fe27831f45bb64e9f6b7e257363f710b342
Mahmood Iftee <mahmoodiftee@gmail.com>
2023-11-24 00:48:46 +0600

    Project Created

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Button from "./Components/Button/Button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Button text={"GET STURTED"}></Button> ,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
