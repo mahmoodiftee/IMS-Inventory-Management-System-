b66c9702c1c827c938df1655a528970fb0bea001
Mahmood Iftee <mahmoodiftee@gmail.com>
2023-11-28 07:22:19 +0600

    several routed and functionalities has been added for manager role

--- src/Pages/DashboardRoot/Dashboard/Product Management/ProductManagement.jsx
+++ src/Pages/DashboardRoot/Dashboard/Product Management/ProductManagement.jsx
@@ -25,7 +25,6 @@ const ProductManagement = () => {
     const handleDelete = _id => {
         Swal.fire({
             title: 'Are you sure?',
-            text: "You won't be able to revert this!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
